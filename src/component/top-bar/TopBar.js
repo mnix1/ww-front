3618f56276cacf714257939c9c8609355df20a33
mnix1 <mvppl123@gmail.com>
2018-07-19 02:19:53 +0200

    media

--- src/component/top-bar/TopBar.js
+++ src/component/top-bar/TopBar.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './styles.css';
 import {connect} from 'react-redux';
 import {APP_NAME} from "../../lang";
-import logo from '../../media/image/robot5.svg';
+import robo from '../../media/image/robo.svg';
+import szeryf from '../../media/image/szeryf.svg';
+import kitek from '../../media/image/kitek.svg';
+import rumcia from '../../media/image/rumcia.svg';
+import zarowa from '../../media/image/zarowa.svg';
 
 class TopBar extends React.PureComponent {
 
@@ -11,7 +15,11 @@ class TopBar extends React.PureComponent {
         return <div className={styles.topBar}>
             <div className={styles.topBarContent} style={{width: contentWidth}}>
                 <div className={styles.topBarContentValue} style={{fontSize: isSmall ? 24 : 48}}>{APP_NAME[window.activeLang]}</div>
-                <img src={logo} height={isSmall ? 80 : 140}/>
+                <img src={robo} height={isSmall ? 80 : 140}/>
+                <img src={szeryf} height={isSmall ? 80 : 140}/>
+                <img src={kitek} height={isSmall ? 80 : 140}/>
+                <img src={zarowa} height={isSmall ? 80 : 140}/>
+                <img src={rumcia} height={isSmall ? 70 : 120}/>
             </div>
         </div>
     }
