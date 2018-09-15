11a752e2b2a82389dbb340b4e8676a6e556dd76d
mnix1 <mvppl123@gmail.com>
2018-09-15 20:34:46 +0200

    bot fix

--- src/bot/botHelper.js
+++ src/bot/botHelper.js
@@ -73,43 +73,43 @@ export const BOTS = {
         hobbyImpact: 0.25, difficultyImpact: 0.03
     },
     [BOT_4]: {
-        user: 'W4X', pass: '!4W4X',
+        user: 'W4X', pass: '!4botW4X',
         hobbies: [CATEGORY_MEMORY],
         baseWisdomSkill: 0.7, baseMentalSkill: 0.5,
         hobbyImpact: 0.25, difficultyImpact: 0.04
     },
     [BOT_5]: {
-        user: 'pierdołła', pass: '!5pierdołła',
+        user: 'pierdołła', pass: '!5botpierdołła',
         hobbies: [CATEGORY_LYRICS],
         baseWisdomSkill: 0.55, baseMentalSkill: 0.6,
         hobbyImpact: 0.15, difficultyImpact: 0.01
     },
     [BOT_6]: {
-        user: 'Kanar', pass: '!6Kanar',
+        user: 'Kanar', pass: '!6botKanar',
         hobbies: [CATEGORY_ELEMENT, CATEGORY_NUMBER],
         baseWisdomSkill: 0.9, baseMentalSkill: 0.6,
         hobbyImpact: 0.09, difficultyImpact: 0.08
     },
     [BOT_7]: {
-        user: 'Best19', pass: '!7Best19',
+        user: 'Best19', pass: '!7botBest19',
         hobbies: [CATEGORY_COUNTRY, CATEGORY_COLOR],
         baseWisdomSkill: 0.5, baseMentalSkill: 0.9,
         hobbyImpact: 0.34, difficultyImpact: 0.01
     },
     [BOT_8]: {
-        user: 'xxULAxx', pass: '!8xxULAxx',
+        user: 'xxULAxx', pass: '!8botxxULAxx',
         hobbies: [CATEGORY_RIDDLE, CATEGORY_MEMORY],
         baseWisdomSkill: 0.77, baseMentalSkill: 0.77,
         hobbyImpact: 0.24, difficultyImpact: 0.06
     },
     [BOT_9]: {
-        user: 'qq5', pass: '!9qq5',
+        user: 'qq5', pass: '!9botqq5',
         hobbies: [CATEGORY_NUMBER, CATEGORY_LYRICS],
         baseWisdomSkill: 0.62, baseMentalSkill: 0.67,
         hobbyImpact: 0.22, difficultyImpact: 0.02
     },
     [BOT_10]: {
-        user: 'radosny1982', pass: '!10radosny1982',
+        user: 'radosny1982', pass: '!10botradosny1982',
         hobbies: [CATEGORY_EQUATION, CATEGORY_TIME],
         baseWisdomSkill: 0.82, baseMentalSkill: 0.77,
         hobbyImpact: 0.05, difficultyImpact: 0.05
