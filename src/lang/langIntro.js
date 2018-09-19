69effb4db34164a6f0aa7b0c7e62453ae197110a
mnix1 <mvppl123@gmail.com>
2018-09-19 22:18:09 +0200

    lang intro eng

--- src/lang/langIntro.js
+++ src/lang/langIntro.js
@@ -42,7 +42,8 @@ import {
     INTRO_STEP_WISIE_DETAILS_TEXT_0,
     INTRO_STEP_WISIE_DETAILS_TEXT_1,
     INTRO_STEP_WISIE_DETAILS_TEXT_2,
-    INTRO_STEP_WISIE_DETAILS_TEXT_3, PICK_WISIE_COUNT,
+    INTRO_STEP_WISIE_DETAILS_TEXT_3,
+    PICK_WISIE_COUNT,
 } from "../content/intro/introHelper";
 
 export function getIntroText(id) {
@@ -88,7 +89,7 @@ const INTRO_TEXT = {
         [INTRO_STEP_WISIE_DETAILS_TEXT_0]: 'Widzimy teraz statystyki Twojego wiedzaka.',
         [INTRO_STEP_WISIE_DETAILS_TEXT_1]: 'W tym miejscu możesz je ulepszać za cenę punktów wiedzy.',
         [INTRO_STEP_WISIE_DETAILS_TEXT_2]: 'Aby to zrobić kliknij proszę na znak',
-        [INTRO_STEP_WISIE_DETAILS_TEXT_3]: 'lub jeśli nie chcesz nic ulepszać, kliknij przycisk "Kontynuuj".',
+        [INTRO_STEP_WISIE_DETAILS_TEXT_3]: 'lub jeśli nie chcesz nic ulepszać, kliknij przycisk"Kontynuuj".',
 
         [INTRO_STEP_WISIE_DETAILS_CLOSE_TEXT_0]: 'Przejdziemy teraz do wyboru reszty wiedzaków do Twojej drużyny.',
         [INTRO_STEP_WISIE_DETAILS_CLOSE_TEXT_1]: `Będziesz mógł wybrać ${PICK_WISIE_COUNT}, która najbardziej Ci się podoba.`,
@@ -98,7 +99,7 @@ const INTRO_TEXT = {
         [INTRO_STEP_GO_TO_EDIT_TEAM_TEXT_1]: 'Proszę kliknij tutaj, aby przejść do ekranu edycji drużyny.',
 
         [INTRO_STEP_EDIT_TEAM_TEXT_0]: 'Teraz możesz utworzyć swoją drużynę.',
-        [INTRO_STEP_EDIT_TEAM_TEXT_1]: 'Proszę wybierz 4 wiedzaki, a następnie kliknij przycisk "Zapisz".',
+        [INTRO_STEP_EDIT_TEAM_TEXT_1]: 'Proszę wybierz 4 wiedzaki, a następnie kliknij przycisk"Zapisz".',
 
         [INTRO_STEP_GO_TO_APP_FROM_WISIE_TEXT_0]: 'Twoja drużyna została utworzona i ma się dobrze ;)',
         [INTRO_STEP_GO_TO_APP_FROM_WISIE_TEXT_1]: 'Czeka na Was wiele przygód. Możecie udać się na kampanię lub wojnę, aby konkurować z innymi.',
@@ -107,6 +108,59 @@ const INTRO_TEXT = {
         [INTRO_STEP_GO_TO_APP_FROM_WISIE_TEXT_4]: 'Do zobaczenia wkrótce ;)',
     },
     [ENGLISH]: {
-        [INTRO_STEP_WELCOME_TEXT_0]: 'Leaflet',
+        [INTRO_STEP_WELCOME_TEXT_0]: "Welcome in Wisiemania Game!",
+        [INTRO_STEP_WELCOME_TEXT_1]: "Let me show you around and give you some tips.",
+        [INTRO_STEP_WELCOME_TEXT_2]: "Here comes the first one:",
+        [INTRO_STEP_WELCOME_TEXT_3]: "To go to the main menu just click Wisiemania at the top of the screen whenever you want.",
+        [INTRO_STEP_WELCOME_TEXT_4]: "If you want previous page just press back button in your explorer or device.",
+
+        [INTRO_STEP_GO_TO_OPTIONS_TEXT_0]: "Click here to open the settings page.",
+
+        [INTRO_STEP_OPTIONS_TEXT_0]: "In Settings you can change your nickname and your Wisor.",
+        [INTRO_STEP_OPTIONS_TEXT_1]: "You can change your nickname right now if you wish.",
+        [INTRO_STEP_OPTIONS_TEXT_2]: "Otherwise press continue.",
+
+        [INTRO_STEP_GO_TO_WISOR_TEXT_0]: "Please click here to choose your Wisor.",
+        [INTRO_STEP_GO_TO_WISOR_TEXT_1]: "You can do it by pressing it's photo.",
+
+        [INTRO_STEP_GO_TO_APP_FROM_OPTIONS_TEXT_0]: "Very nice ;)",
+        [INTRO_STEP_GO_TO_APP_FROM_OPTIONS_TEXT_1]: "Your Wisor will guide you further.",
+        [INTRO_STEP_GO_TO_APP_FROM_OPTIONS_TEXT_2]: "Wisor awaits you in main menu.",
+        [INTRO_STEP_GO_TO_APP_FROM_OPTIONS_TEXT_3]: "Good luck, nice meeting you :D",
+
+        [INTRO_STEP_GO_TO_WISIES_TEXT_0]: "Hi ^_^!",
+        [INTRO_STEP_GO_TO_WISIES_TEXT_1]: "Thanks for the trust :)",
+        [INTRO_STEP_GO_TO_WISIES_TEXT_2]: "How about learning some stuff about Wisies?",
+        [INTRO_STEP_GO_TO_WISIES_TEXT_3]: "Press here once you're ready.",
+
+        [INTRO_STEP_EXPERIMENT_TEXT_0]: "Wisies are your students, you can train them and teach new skills.",
+        [INTRO_STEP_EXPERIMENT_TEXT_1]: "To gather more and more Wisies you do scientific experiments.",
+        [INTRO_STEP_EXPERIMENT_TEXT_2]: "Let's run one experiment now.",
+
+        [INTRO_STEP_NEW_WISIE_TEXT_0]: "Congrats! That's your first Wisie ;)",
+        [INTRO_STEP_NEW_WISIE_TEXT_1]: "In the top right corner are Wisie's interests.",
+        [INTRO_STEP_NEW_WISIE_TEXT_2]: "On the left is mean value fo Wisie's statistics.",
+        [INTRO_STEP_NEW_WISIE_TEXT_3]: "Click on Wisie for more details.",
+
+        [INTRO_STEP_WISIE_DETAILS_TEXT_0]: "Now you can see all your Wisie's statistics.",
+        [INTRO_STEP_WISIE_DETAILS_TEXT_1]: "Here you can upgrade them in exchange for Wisdom Points.",
+        [INTRO_STEP_WISIE_DETAILS_TEXT_2]: "To do so, please click",
+        [INTRO_STEP_WISIE_DETAILS_TEXT_3]: "or just press 'Continue'.",
+
+        [INTRO_STEP_WISIE_DETAILS_CLOSE_TEXT_0]: "Now let's choose the rest of Wisies for your Team.",
+        [INTRO_STEP_WISIE_DETAILS_CLOSE_TEXT_1]: `You select those ${PICK_WISIE_COUNT} you prefer.`,
+        [INTRO_STEP_WISIE_DETAILS_CLOSE_TEXT_2]: "Press here to close statistics window.",
+
+        [INTRO_STEP_GO_TO_EDIT_TEAM_TEXT_0]: "Congrats! You have enough Wisies to make a Team.",
+        [INTRO_STEP_GO_TO_EDIT_TEAM_TEXT_1]: "Press to edit your Team.",
+
+        [INTRO_STEP_EDIT_TEAM_TEXT_0]: "Now you can set up the Team.",
+        [INTRO_STEP_EDIT_TEAM_TEXT_1]: "Select 4 Wisies and press 'Save'.",
+
+        [INTRO_STEP_GO_TO_APP_FROM_WISIE_TEXT_0]: "Your Team has been set up and is doing well ;)",
+        [INTRO_STEP_GO_TO_APP_FROM_WISIE_TEXT_1]: "There are plenty adventures waiting for you guys. You can go to campain or war to compete with others.",
+        [INTRO_STEP_GO_TO_APP_FROM_WISIE_TEXT_2]: "It's a pleasure working with you :D",
+        [INTRO_STEP_GO_TO_APP_FROM_WISIE_TEXT_3]: "That's all I wanted to show you for now.",
+        [INTRO_STEP_GO_TO_APP_FROM_WISIE_TEXT_4]: "See you ;)",
     }
 };
