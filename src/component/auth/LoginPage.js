e809249d779e1a11b2daab720fc9224ce051f961
mnix1 <mvppl123@gmail.com>
2018-11-19 13:45:15 +0100

    facebook login

--- src/component/auth/LoginPage.js
+++ src/component/auth/LoginPage.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import {connect} from "react-redux";
-import googleLogo from '../../media/image/icon/googleLogo.svg';
+import {FaFacebook, FaGoogle} from 'react-icons/fa';
 import {getText, TEXT_LOGIN} from "../../lang/langText";
 import {CREAM_COLOR} from "../../util/style/constant";
+import {getFullWisor, getIntroWisor, getWisor} from "../../util/wisorHelper";
 
 class LoginPage extends React.PureComponent {
 
+    renderButton(href, backgroundColor, text, logo) {
+        return <div className='justifyCenter paddingTopRem'>
+            <a href={href} className='justifyStart paddingRem borderRadiusRem'
+               style={{color: CREAM_COLOR, textDecoration: 'none', backgroundColor, width: '10rem'}}>
+                <div className='justifyCenter'>
+                    {logo}
+                </div>
+                <div className='justifyCenter width100'>
+                    <div className='justifyCenter flexColumn paddingLeftRem fontSize08Rem'>
+                        {text}
+                    </div>
+                </div>
+            </a>
+        </div>
+    }
+
     render() {
         const {screen} = this.props;
-        return <div className='page loginPage' style={{height: screen.contentHeight / 2, width: screen.contentWidth}}>
+        return <div className='page loginPage' style={{width: screen.contentWidth}}>
             <div className='pageContent overflowAuto'>
-                <div className='absoluteBackgroundMix blackBackground'/>
+                <div className='absoluteBackgroundMix blackBackground' style={{opacity: 0.7}}/>
                 <div className='flex flexColumn height100 relative'>
                     <div className='justifyCenter paddingTopRem'>{getText(TEXT_LOGIN)}</div>
-                    <div className='justifyCenter flexColumn height100'>
-                        <div className='justifyCenter'>
-                            <a href="/_login/google" className='justifyCenter boxShadow paddingRem'
-                               style={{color: CREAM_COLOR, textDecoration: 'none'}}>
-                                <img alt='' src={googleLogo} height={30}/>
-                                <div className='justifyCenter flexColumn paddingLeftRem'>Google</div>
-                            </a>
+                    <div className='justifyCenter'>
+                        <img className='paddingRightRem' src={getIntroWisor()} height={screen.contentHeight / 2}/>
+                        <div className='justifyCenter flexColumn paddingLeftRem'>
+                            {this.renderButton('/_login/google', '#4285f4', 'Log in with Google', <FaGoogle/>)}
+                            {this.renderButton('/_login/facebook', '#4267b2', 'Log in with Facebook', <FaFacebook/>)}
                         </div>
                     </div>
                 </div>
